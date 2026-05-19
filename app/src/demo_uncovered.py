a76313b30c535e0a15f3840bfc3eb2666df3d3d0
Naxeel <ilya.general64@gmail.com>
2026-05-19 21:47:34 +0400

    Coverage lower  50%

---
+++ app/src/demo_uncovered.py
@@ -0,0 +1,89 @@
+def demo_uncovered_01():
+    return 1
+
+def demo_uncovered_02():
+    return 2
+
+def demo_uncovered_03():
+    return 3
+
+def demo_uncovered_04():
+    return 4
+
+def demo_uncovered_05():
+    return 5
+
+def demo_uncovered_06():
+    return 6
+
+def demo_uncovered_07():
+    return 7
+
+def demo_uncovered_08():
+    return 8
+
+def demo_uncovered_09():
+    return 9
+
+def demo_uncovered_10():
+    return 10
+
+def demo_uncovered_11():
+    return 11
+
+def demo_uncovered_12():
+    return 12
+
+def demo_uncovered_13():
+    return 13
+
+def demo_uncovered_14():
+    return 14
+
+def demo_uncovered_15():
+    return 15
+
+def demo_uncovered_16():
+    return 16
+
+def demo_uncovered_17():
+    return 17
+
+def demo_uncovered_18():
+    return 18
+
+def demo_uncovered_19():
+    return 19
+
+def demo_uncovered_20():
+    return 20
+
+def demo_uncovered_21():
+    return 21
+
+def demo_uncovered_22():
+    return 22
+
+def demo_uncovered_23():
+    return 23
+
+def demo_uncovered_24():
+    return 24
+
+def demo_uncovered_25():
+    return 25
+
+def demo_uncovered_26():
+    return 26
+
+def demo_uncovered_27():
+    return 27
+
+def demo_uncovered_28():
+    return 28
+
+def demo_uncovered_29():
+    return 29
+
+def demo_uncovered_30():
+    return 30
