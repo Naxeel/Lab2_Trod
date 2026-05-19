f7c53f1bb1fb946c6eb3b869345f69d815c82a8c
Naxeel <ilya.general64@gmail.com>
2026-05-19 22:00:37 +0400

    demo: show CI fails when coverage is below 50%

--- app/src/demo_uncovered.py
+++ app/src/demo_uncovered.py
@@ -1,89 +1,478 @@
-def demo_uncovered_01():
+def demo_uncovered_001():
     return 1
 
-def demo_uncovered_02():
+
+def demo_uncovered_002():
     return 2
 
-def demo_uncovered_03():
+
+def demo_uncovered_003():
     return 3
 
-def demo_uncovered_04():
+
+def demo_uncovered_004():
     return 4
 
-def demo_uncovered_05():
+
+def demo_uncovered_005():
     return 5
 
-def demo_uncovered_06():
+
+def demo_uncovered_006():
     return 6
 
-def demo_uncovered_07():
+
+def demo_uncovered_007():
     return 7
 
-def demo_uncovered_08():
+
+def demo_uncovered_008():
     return 8
 
-def demo_uncovered_09():
+
+def demo_uncovered_009():
     return 9
 
-def demo_uncovered_10():
+
+def demo_uncovered_010():
     return 10
 
-def demo_uncovered_11():
+
+def demo_uncovered_011():
     return 11
 
-def demo_uncovered_12():
+
+def demo_uncovered_012():
     return 12
 
-def demo_uncovered_13():
+
+def demo_uncovered_013():
     return 13
 
-def demo_uncovered_14():
+
+def demo_uncovered_014():
     return 14
 
-def demo_uncovered_15():
+
+def demo_uncovered_015():
     return 15
 
-def demo_uncovered_16():
+
+def demo_uncovered_016():
     return 16
 
-def demo_uncovered_17():
+
+def demo_uncovered_017():
     return 17
 
-def demo_uncovered_18():
+
+def demo_uncovered_018():
     return 18
 
-def demo_uncovered_19():
+
+def demo_uncovered_019():
     return 19
 
-def demo_uncovered_20():
+
+def demo_uncovered_020():
     return 20
 
-def demo_uncovered_21():
+
+def demo_uncovered_021():
     return 21
 
-def demo_uncovered_22():
+
+def demo_uncovered_022():
     return 22
 
-def demo_uncovered_23():
+
+def demo_uncovered_023():
     return 23
 
-def demo_uncovered_24():
+
+def demo_uncovered_024():
     return 24
 
-def demo_uncovered_25():
+
+def demo_uncovered_025():
     return 25
 
-def demo_uncovered_26():
+
+def demo_uncovered_026():
     return 26
 
-def demo_uncovered_27():
+
+def demo_uncovered_027():
     return 27
 
-def demo_uncovered_28():
+
+def demo_uncovered_028():
     return 28
 
-def demo_uncovered_29():
+
+def demo_uncovered_029():
     return 29
 
-def demo_uncovered_30():
+
+def demo_uncovered_030():
     return 30
+
+
+def demo_uncovered_031():
+    return 31
+
+
+def demo_uncovered_032():
+    return 32
+
+
+def demo_uncovered_033():
+    return 33
+
+
+def demo_uncovered_034():
+    return 34
+
+
+def demo_uncovered_035():
+    return 35
+
+
+def demo_uncovered_036():
+    return 36
+
+
+def demo_uncovered_037():
+    return 37
+
+
+def demo_uncovered_038():
+    return 38
+
+
+def demo_uncovered_039():
+    return 39
+
+
+def demo_uncovered_040():
+    return 40
+
+
+def demo_uncovered_041():
+    return 41
+
+
+def demo_uncovered_042():
+    return 42
+
+
+def demo_uncovered_043():
+    return 43
+
+
+def demo_uncovered_044():
+    return 44
+
+
+def demo_uncovered_045():
+    return 45
+
+
+def demo_uncovered_046():
+    return 46
+
+
+def demo_uncovered_047():
+    return 47
+
+
+def demo_uncovered_048():
+    return 48
+
+
+def demo_uncovered_049():
+    return 49
+
+
+def demo_uncovered_050():
+    return 50
+
+
+def demo_uncovered_051():
+    return 51
+
+
+def demo_uncovered_052():
+    return 52
+
+
+def demo_uncovered_053():
+    return 53
+
+
+def demo_uncovered_054():
+    return 54
+
+
+def demo_uncovered_055():
+    return 55
+
+
+def demo_uncovered_056():
+    return 56
+
+
+def demo_uncovered_057():
+    return 57
+
+
+def demo_uncovered_058():
+    return 58
+
+
+def demo_uncovered_059():
+    return 59
+
+
+def demo_uncovered_060():
+    return 60
+
+
+def demo_uncovered_061():
+    return 61
+
+
+def demo_uncovered_062():
+    return 62
+
+
+def demo_uncovered_063():
+    return 63
+
+
+def demo_uncovered_064():
+    return 64
+
+
+def demo_uncovered_065():
+    return 65
+
+
+def demo_uncovered_066():
+    return 66
+
+
+def demo_uncovered_067():
+    return 67
+
+
+def demo_uncovered_068():
+    return 68
+
+
+def demo_uncovered_069():
+    return 69
+
+
+def demo_uncovered_070():
+    return 70
+
+
+def demo_uncovered_071():
+    return 71
+
+
+def demo_uncovered_072():
+    return 72
+
+
+def demo_uncovered_073():
+    return 73
+
+
+def demo_uncovered_074():
+    return 74
+
+
+def demo_uncovered_075():
+    return 75
+
+
+def demo_uncovered_076():
+    return 76
+
+
+def demo_uncovered_077():
+    return 77
+
+
+def demo_uncovered_078():
+    return 78
+
+
+def demo_uncovered_079():
+    return 79
+
+
+def demo_uncovered_080():
+    return 80
+
+
+def demo_uncovered_081():
+    return 81
+
+
+def demo_uncovered_082():
+    return 82
+
+
+def demo_uncovered_083():
+    return 83
+
+
+def demo_uncovered_084():
+    return 84
+
+
+def demo_uncovered_085():
+    return 85
+
+
+def demo_uncovered_086():
+    return 86
+
+
+def demo_uncovered_087():
+    return 87
+
+
+def demo_uncovered_088():
+    return 88
+
+
+def demo_uncovered_089():
+    return 89
+
+
+def demo_uncovered_090():
+    return 90
+
+
+def demo_uncovered_091():
+    return 91
+
+
+def demo_uncovered_092():
+    return 92
+
+
+def demo_uncovered_093():
+    return 93
+
+
+def demo_uncovered_094():
+    return 94
+
+
+def demo_uncovered_095():
+    return 95
+
+
+def demo_uncovered_096():
+    return 96
+
+
+def demo_uncovered_097():
+    return 97
+
+
+def demo_uncovered_098():
+    return 98
+
+
+def demo_uncovered_099():
+    return 99
+
+
+def demo_uncovered_100():
+    return 100
+
+
+def demo_uncovered_101():
+    return 101
+
+
+def demo_uncovered_102():
+    return 102
+
+
+def demo_uncovered_103():
+    return 103
+
+
+def demo_uncovered_104():
+    return 104
+
+
+def demo_uncovered_105():
+    return 105
+
+
+def demo_uncovered_106():
+    return 106
+
+
+def demo_uncovered_107():
+    return 107
+
+
+def demo_uncovered_108():
+    return 108
+
+
+def demo_uncovered_109():
+    return 109
+
+
+def demo_uncovered_110():
+    return 110
+
+
+def demo_uncovered_111():
+    return 111
+
+
+def demo_uncovered_112():
+    return 112
+
+
+def demo_uncovered_113():
+    return 113
+
+
+def demo_uncovered_114():
+    return 114
+
+
+def demo_uncovered_115():
+    return 115
+
+
+def demo_uncovered_116():
+    return 116
+
+
+def demo_uncovered_117():
+    return 117
+
+
+def demo_uncovered_118():
+    return 118
+
+
+def demo_uncovered_119():
+    return 119
+
+
+def demo_uncovered_120():
+    return 120
